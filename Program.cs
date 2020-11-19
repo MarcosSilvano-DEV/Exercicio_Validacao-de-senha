ba61c91ad2e5b6d80232e37003a28ce0b6144fef
Marcos Silvano <marcossilvano@gmail.com>
2020-11-19 17:03:55 -0300

    Validação de senha concluido

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio_Validacao_de_senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Validação de senha");
+         Console.WriteLine("------------------");
+
+         Console.Write("Digite o seu nome: ");
+         string nome = Console.ReadLine();
+
+         Console.Write("Digite a senha: ");
+         string senha = Console.ReadLine();
+
+
+         while(nome == senha){
+            Console.WriteLine("Senha inválida - Digite uma senha diferente do seu nome.");
+            Console.Write("Digite a senha: ");
+            senha = Console.ReadLine();
+        }
+        Console.WriteLine("Usuário cadastrado com sucesso!");
+        }
+    }
+}
